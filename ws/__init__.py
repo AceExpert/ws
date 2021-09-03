c0a7c58e91c132213eccbec8b80033f953a8f2f5
AceExpert <very.anshul@gmail.com>
2021-09-03 09:47:22 +0530

    Update __init__ to reflect model module changes

--- ws/__init__.py
+++ ws/__init__.py
@@ -1,8 +1,7 @@
 from .client import ClientSocket
 from .server import ServerSocket
-from .models import Message
+from .models import Message, WebSocketData
 from .wsprotocols import WSSProtocol, WSCProtocol
-from .models.data import WebSocketData
 
 __version__ = "1.0"
 __author__ = "Cybertron"
