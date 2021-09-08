7a6e3fd84a09382dbcabffd267bbfa34183be955
AceExpert <very.anshul@gmail.com>
2021-09-08 09:02:45 +0530

    update library version to 1.9.0

--- ws/__init__.py
+++ ws/__init__.py
@@ -4,5 +4,5 @@ from .models import Message, Object
 from .wsprotocols import WSSProtocol, WSCProtocol
 from .exceptions import ParameterConflict
 
-__version__ = "1.5.0"
+__version__ = "1.9.0"
 __author__ = "Cybertron"
