1e9114dba2182c42b4839a8fa038b8070933b4fa
Jabbey92 <32350542+Jabbey92@users.noreply.github.com>
2022-04-11 10:52:30 -0500

    Reduced Imports and added __slots__

--- ws/collector.py
+++ ws/collector.py
@@ -1,18 +1,22 @@
-import typing, datetime, asyncio
+from datetime import datetime
+from typing import Callable, List, Any
+from asyncio import TimeoutError
 
 class EventCollector:
+    __slots__ = '__websocket', '__time'
+    
     def __init__(self, websocket, time: float) -> None:
         self.__websocket = websocket
         self.__time = time
-    async def collect(self, event: str, check: typing.Callable = None) -> typing.List[typing.Any]:
+    async def collect(self, event: str, check: Callable = None) -> List[Any]:
         collected = []
         start_time = datetime.datetime.utcnow()
-        while (datetime.datetime.utcnow() - start_time).total_seconds() < self.__time:
+        while (datetime.utcnow() - start_time).total_seconds() < self.__time:
             try:
                 event_data = await self.__websocket.wait_for(event, 
                                                              check=check, 
                                                              timeout=self.__time - (datetime.datetime.utcnow()-start_time).total_seconds())                
                 collected.append(event_data)
-            except asyncio.TimeoutError:
+            except TimeoutError:
                 pass
         return collected
